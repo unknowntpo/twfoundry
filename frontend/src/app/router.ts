2b427448804ea55515426e0f8d0c9207ebc069e4
Eric Chang <e850506@gmail.com>
2026-04-30 11:41:53 +0800

    Revert "Replace home with voxel cockpit"
    This reverts commit 571653dbd7db1137885a37b2935156131155286a.

--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DesignSystemPage from "@/features/design-system/components/DesignSystemPage.vue";
 import MrtDashboard from "@/features/mrt/components/MrtDashboard.vue";
-import VoxelCockpitPage from "@/features/voxel-cockpit/components/VoxelCockpitPage.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
-      name: "voxel-cockpit",
-      component: VoxelCockpitPage,
-    },
-    {
-      path: "/mrt-dashboard",
       name: "mrt-dashboard",
       component: MrtDashboard,
     },
