cc6fb94cfd4736175232b16e949089a48073d8bf
Eric Chang <e850506@gmail.com>
2026-04-30 11:55:51 +0800

    Promote Sakura voxel experiment to frontend

--- frontend/src/features/twfoundry-demo/main.ts
+++
@@ -1,5 +0,0 @@
-import "@/style.css";
-import { createApp } from "vue";
-import TwfoundryDemo from "./components/TwfoundryDemo.vue";
-
-createApp(TwfoundryDemo).mount("#twfoundry-demo");
--- frontend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createPinia } from "pinia";
-import { createApp } from "vue";
-import App from "./app/App.vue";
-import { router } from "./app/router";
-import { i18n } from "./shared/i18n";
-import "./style.css";
-
-createApp(App).use(createPinia()).use(router).use(i18n).mount("#app");
